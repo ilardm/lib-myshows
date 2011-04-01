9003cb79db57bdde57700933aa4ff9967614af09
Ilya Arefiev <arefiev.id@gmail.com>
2011-04-01 23:26:58 +0400

    add dummy interface for API (MyshowsClient)

--- src/aid/lib/myshows/MyshowsAPI.java
+++ src/aid/lib/myshows/MyshowsAPI.java
@@ -59,7 +59,7 @@ public class MyshowsAPI {
 	
 	protected HttpClient httpClient=null;
 	
-	public MyshowsAPI(String _user, String _password) {
+	protected MyshowsAPI(String _user, String _password) {
 		user=_user;
 		
 		// get md5 hash of password
@@ -96,7 +96,7 @@ public class MyshowsAPI {
 		}
 	}
 	
-	public boolean login() {
+	protected boolean login() {
 		
 		if ( httpClient==null ) {
 			// debug
@@ -140,7 +140,7 @@ public class MyshowsAPI {
 		return false;
 	}
 	
-	public String getShows() {
+	protected String getShows() {
 		if ( httpClient==null ) {
 			return null;
 		}
@@ -179,7 +179,7 @@ public class MyshowsAPI {
 		return null;
 	}
 	
-	public String getUnwatchedEpisodes() {
+	protected String getUnwatchedEpisodes() {
 		if ( httpClient==null ) {
 			return null;
 		}
@@ -218,7 +218,7 @@ public class MyshowsAPI {
 		return null;
 	}
 	
-	public String getNextEpisodes() {
+	protected String getNextEpisodes() {
 		if ( httpClient==null ) {
 			return null;
 		}
@@ -257,7 +257,7 @@ public class MyshowsAPI {
 		return null;
 	}
 	
-	public String getSeenEpisodes(int _show) {
+	protected String getSeenEpisodes(int _show) {
 		if ( httpClient==null || _show<0 ) {
 			return null;
 		}
@@ -297,7 +297,7 @@ public class MyshowsAPI {
 		return null;
 	}
 
-	public boolean checkEpisode(int _episode) {
+	protected boolean checkEpisode(int _episode) {
 		return checkEpisode(_episode, -1);
 	}
 	
@@ -307,7 +307,7 @@ public class MyshowsAPI {
 	 * @param _ratio if ( _ratio<0 ) { no ratio using }
 	 * @return true anyway :( except unauthorized
 	 */
-	public boolean checkEpisode(int _episode, int _ratio) {
+	protected boolean checkEpisode(int _episode, int _ratio) {
 		
 		if ( httpClient==null || _episode<0 ) {
 			// debug
@@ -355,7 +355,7 @@ public class MyshowsAPI {
 		return false;
 	}
 
-	public boolean unCheckEpisode(int _episode) {
+	protected boolean unCheckEpisode(int _episode) {
 		
 		if ( httpClient==null || _episode<0 ) {
 			// debug
