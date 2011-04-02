a46ba0b3b570cbca154057212beca4e9357d436f
Ilya Arefiev <arefiev.id@gmail.com>
2011-04-03 01:46:12 +0400

    add docs

--- src/aid/lib/myshows/MyshowsClient.java
+++ src/aid/lib/myshows/MyshowsClient.java
@@ -36,13 +36,18 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
- * 
+ * MyShows API public Client
+ * @see <a href="http://api.myshows.ru/">http://api.myshows.ru/</a>
  * @author Ilya Arefiev (arefiev.id@gmail.com)
  *
  */
 public class MyshowsClient {
 	protected MyshowsAPI api=null;
 	
+	/**
+	 * dummy constructor<br>
+	 * just creates {@link MyshowsAPI}
+	 */
 	public MyshowsClient() {
 		api=new MyshowsAPI();
 		
@@ -51,18 +56,54 @@ public class MyshowsClient {
 		// TODO: check if offline
 	}
 	
+	/**
+	 * login into username's account<br>
+	 * calls <code>MyshowsAPI.login()</code>
+	 * @param _username
+	 * @param _password
+	 * @return <code>true</code> if success<br>
+	 * 			<code>false</code> otherwise 
+	 */
 	public boolean login(String _username, String _password) {
 		System.out.println("+++ login(String "+_username+", String "+_password+")");
 		
 		return api.login(_username, _password);
 	}
 	
+	/**
+	 * logout from user's account with clearing auth info<br>
+	 * <b>currently does nothing</b>
+	 * @return currently <code>false</code>
+	 */
 	public boolean logout() {
+		// TODO: implement logout @ API && client
 		System.out.println("+++ logout()");
 		
 		return false;
 	}
 	
+	/**
+	 * get all shows (watching, canceled, etc) of user<br>
+	 * calls <code>MyshowsAPI.getShows()</code><br>
+	 * 
+	 * {@link JSONArray} format:
+		<pre>[
+  {
+    "rating": 0,
+    "ruTitle": "$translated_title",
+    "runtime": 43,	// TODO
+    "showId": $showId,
+    "showStatus": "Returning Series", // TODO: variants
+    "title": "$original_title",
+    "totalEpisodes": $num_of_totlat_episodes,
+    "watchStatus": "$user's_watching_status",	// TODO: variants 
+    "watchedEpisodes": $num_of_watched_episodes
+  }
+]
+		</pre>
+	 * @return {@link JSONArray} with shows if success<br>
+	 * 			<code>null</code> otherwise
+	 */
 	public JSONArray getShows() {
 		System.out.println("+++ getShows()");
 		
@@ -99,6 +140,27 @@ public class MyshowsClient {
 		return ret;
 	}
 	
+	/**
+	 * get all unwatched episodes of all user's shows or filtered by
+	 * <code>_show</code><br>
+	 * calls <code>MyshowsAPI.getUnwatchedEpisodes()</code> and filter result by
+	 * <code>_show</code> (if <code>_show</code>>0)<br>
+	 * {@link JSONArray} format:
+		<pre>[
+  {
+    "airDate": "$dd.mm.yyyy",
+    "episodeId": $episode_id,
+    "episodeNumber": $episode_number,
+    "seasonNumber": $season_number,
+    "showId": $showId,
+    "title": "$original_episode_title"
+  }
+]
+		</pre>
+	 * @param _show $showId to filter unwatched episodes
+	 * @return {@link JSONArray} with episodes if success<br>
+	 * 			<code>null</code> otherwise
+	 */
 	public JSONArray getUnwatchedEpisodes(int _show) {
 		System.out.println("+++ getUnwatchedEpisodes("+_show+")");
 		
@@ -128,6 +190,8 @@ public class MyshowsClient {
 						ret.put( episode );
 					} else if ( _show<0 ) {
 						ret.put( episode );
+					} else {
+						continue;
 					}
 				}
 				
@@ -142,6 +206,27 @@ public class MyshowsClient {
 		return ret;
 	}
 	
+	/**
+	 * get next (future) episodes of all user's shows or filtered by
+	 * <code>_show</code><br>
+	 * calls <code>MyshowsAPI.getNextEpisodes()</code> and filter result by
+	 * <code>_show</code> (if <code>_show</code>>0)<br>
+	 * {@link JSONArray} format:
+		<pre>[
+  "$episode_id": {
+    "airDate": "$dd.mm.yyyy",
+    "episodeId": $episode_id,
+    "episodeNumber": $episode_number,
+    "seasonNumber": $season_number,
+    "showId": $showId,
+    "title": "$original_episode_title"
+  }
+]
+		</pre>
+	 * @param _show $showId to filter next episodes
+	 * @return {@link JSONArray} with episodes if success<br>
+	 * 			<code>null</code> otherwise
+	 */
 	public JSONArray getNextEpisodes(int _show) {
 		System.out.println("+++ getNextEpisodes("+_show+")");
 		
@@ -187,6 +272,25 @@ public class MyshowsClient {
 		return ret;
 	}
 	
+	/**
+	 * get seen episodes of user's show (given by <code>_show</code>)<br>
+	 * calls <code>MyshowsAPI.getSeenEpisodes()</code><br>
+	 * {@link JSONArray} format:
+		<pre>[
+  "$episode_id": {
+    "airDate": "$dd.mm.yyyy",
+    "episodeId": $episode_id,
+    "episodeNumber": $episode_number,
+    "seasonNumber": $season_number,
+    "showId": $showId,
+    "title": "$original_episode_title"
+  }
+]
+		</pre>
+	 * @param _show $showId to filter next episodes
+	 * @return {@link JSONArray} with episodes if success<br>
+	 * 			<code>null</code> otherwise
+	 */
 	public JSONArray getSeenEpisodes(int _show) {
 		System.out.println("+++ getSeenEpisodes(int "+_show+")");
 		
@@ -223,18 +327,41 @@ public class MyshowsClient {
 		return ret;
 	}
 	
+	/**
+	 * mark episode as watched<br>
+	 * calls <code>MyshowsAPI.checkEpisode(_episode)</code><br>
+	 * @param _episode $episode_id
+	 * @return <code>true</code> if success<br>
+	 * 			<code>false</code> otherwise (likely unauthorized)
+	 */
 	public boolean checkEpisode(int _episode) {
 		System.out.println("+++ checkEpisode(int "+_episode+")");
 		
 		return api.checkEpisode(_episode);
 	}
 	
+	
+	/**
+	 * mark episode as watched with ratio<br>
+	 * calls <code>MyshowsAPI.checkEpisode(_episode, _ratio)</code>
+	 * @param _episode $episode_id
+	 * @param _ratio $ratio // <b>currently useless</b>
+	 * @return <code>true</code> if success<br>
+	 * 			<code>false</code> otherwise (likely unauthorized)
+	 */
 	public boolean checkEpisode(int _episode, int _ratio) {
 		System.out.println("+++ checkEpisode(int "+_episode+", int "+_ratio+")");
 		
 		return api.checkEpisode(_episode, _ratio);
 	}
 	
+	/**
+	 * mark episode as unwatched<br>
+	 * calls <code>MyshowsAPI.unCheckEpisode(_episode)</code>
+	 * @param _episode $episode_id
+	 * @return <code>true</code> if success<br>
+	 * 			<code>false</code> otherwise (likely unauthorized)
+	 */
 	public boolean unCheckEpisode(int _episode) {
 		System.out.println("+++ unCheckEpisode(int "+_episode+")");
 		
